fc1fe7b57f69413a8a065a52ed4832da2aa05a14
JITHU MATHEWS <jithumathews111@gmail.com>
2022-07-12 12:30:10 +0530

    commit

---
+++ bin/flutter_projects.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_projects/flutter_projects.dart' as flutter_projects;
+
+void main() {
+  String name = "jithu";
+
+  print("my name is ${name}");
+}
---
+++ test/flutter_projects_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_projects/flutter_projects.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
